7396e5cd7714121e3fca761fbac7d65401ab4561
Jorge Herrera <jrgherrera@gmail.com>
2017-05-30 19:22:51 -0600

    Commit inicial

---
+++ js/functions.js
@@ -0,0 +1,245 @@
+/* Variables globales */ 
+// Variables para definir el Canvas.
+var canvas;
+var ctx;
+
+// Tama�o del canvas
+var WIDTH = 600; 
+var HEIGHT = 520; 
+
+// Variables para controlar la posici�n de la pelota.
+var x;
+var y;
+
+// Variables para controlar la direcci�n de la pelota.
+var dx;
+var dy;
+  
+// Variables para controlar la posici�n de la paleta
+var paddleX;
+var paddleY;
+
+// Variables para controlar el nivel de juego en que se encuentra el jugador.
+var level;
+
+// variable para resetear inicio del movimiento de l apaleta
+var interval;
+
+// Variables per a reproducir el audio. No estan definidas porqu� en funci�n de la soluci�n aportada pueden hacer falta m�s o menos variables.
+// ...
+
+loadData();
+
+$('#start').on('click', function(e) {
+  clearInterval(interval);
+  init_game();
+})
+
+$(document).on('keydown', function(e){
+  if (e.keyCode == 37 && paddleX !== 0){	// Flecha izquierda del teclado.
+    paddleX -= 15;
+  }
+
+  if (e.keyCode == 39 && paddleX !== 510){	// Flecha derecha del teclado.
+    paddleX += 15;
+  }
+
+});
+
+/* 
+Dibujamos un recuadro.
+Cada recuadro tiene las variables de color, la posici�n x, y inicial en el canvas y la anchura y altura del recuadro.
+Esta es una funci�n auxiliar utilizada para dibujar la paleta y que puede ser utilizada tambi�n para dibujar los recuadros de un nivel.
+*/
+function square(color, x, y, width, height) {
+  ctx.beginPath();
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, width, height);
+}
+
+// Funci�n para dibujar la redonda.
+function circle(x,y,r) {
+  ctx.beginPath();
+  ctx.fillStyle="#F80";
+  ctx.arc(x, y, r, 0, Math.PI*2, true);
+  ctx.fill();
+}
+
+
+/*
+Esta funci�n verifica que el juego ha llegado al final de una partida.
+En esta funci�n pueden pasar diferentes ccosas:
+  1. Que el usuario pierda la partida porqu� la pelota toque el fondo del canvas.
+  2. que el usuario haya tocado todas las pelotas:
+    2a. Si nos encontramos el primer nivel de la partida se ha de tener presente que tenemos que pasar al segundo nivel.
+    2b. Se ha acabo el juego. Se ha de mostrar al usuario el mensaje de finalizaci�n del juego.
+*/
+function end_of_game(){
+
+}
+/*	
+En esta funcion se tienen que dibujar los recuadros que forman parte de un nivel.
+El nivel a mostrar se pasa com par�metro de la funci�n.
+Para dibujar cada uno de los recuadros se puede utilizar la funci� auxiliar "square" utilizada tambi�n para dibujar la paleta.
+*/
+function draw_level(level) {
+  if (level == 1) active_level = squares_level1;
+  if (level == 2) active_level = squares_level2;
+
+  var N = active_level.length;
+  for (i=0; i<N ; i++){
+    if (active_level[i].active){
+      //Dibujamos solo los cuadros que están activos.
+      square(active_level[i].color, active_level[i].x, active_level[i].y, active_level[i].w, active_level[i].h);
+    }
+  }
+}
+
+function ball_touch_wall () {
+  // cuando la bola toca la pared derecha
+  if (x === 600) {
+    dx = -5;
+  }
+  // cuando la bola toca la pared izquierda
+  if (x === 0) {
+    dx = 5;
+  }
+  // cuando la bola toca el fondo
+  if (y === 520) {
+    //perdio
+  }
+  // cuando la bola toca el techo
+  if (y === 0) {
+    dy = 5;
+  }
+}
+
+/*  	
+En esta funci�n se tiene que verificar si la pelota est� tocando alguno de los recuadros.
+En caso de que la pelota toque alguno de los recuadros, se debe tener presente que le recuadro no se tiene que volver a pintar
+por pantalla hasta que no se vuellva a iniciar una aprtida.
+Se debe tener en cuenta el nivel ( 1 o 2 ) en que esta jugando el jugador.
+*/
+function ball_touch_square (level) {
+  if (level == 1) active_level = squares_level1;
+  if (level == 2) active_level = squares_level2;
+
+  var N = active_level.length;
+  for (i=0; i<N ; i++) {
+    var s = active_level[i];
+    if (s.active) {
+      var topX = parseInt(s.x);
+      var bottomX = parseInt(s.x) + parseInt(s.w);
+      var topY = parseInt(s.y);
+      var bottomY = parseInt(s.y) + parseInt(s.h);
+      if (x >= topX && x <= bottomX && y >= topY && y <= bottomY) {
+        s.active = false;
+        dy = 5;
+      }
+    }
+  }
+}
+
+/*	
+En esta funci�n se dee detectar si la pelota colisiona con la paleta y que se tiene que hacer en este caso.
+  � Qu� direcci�n tomar� la pelota?
+  � Se debe emitir algun sonido?
+*/
+function ball_touch_paddle () {
+  // posición de la paleta -> paddleX
+  // posición de la bola -> x , y
+  if (x >= paddleX && x <= (paddleX + 90) && y === 490) {
+    dy = -5;
+  }
+}
+
+// Limpiamos el Canvas antes de dibujarlo de nuevo.
+function clear() {
+  ctx.clearRect(0, 0, WIDTH, HEIGHT);
+}
+
+function draw(){
+
+  soundEfx = document.getElementById("soundEfx");
+    
+  // Limpiamos el Canvas antes de dibujarlo de nuevo.
+  clear();
+    
+  // Dibujamos los recuadros de un nivel.
+  draw_level(level);
+
+  // Dibujamos la pelota con un tama�o de 10 px.
+  circle(x, y, 10);
+
+  ball_touch_wall();
+
+  // En esta oarte controlamos si la pelota ha tocado la paleta, y por tanto si ha de cambiar de posici�n o no.
+  ball_touch_paddle();
+
+  /*
+    Ahora controlamos si la pelota ha tocado cualquiera de los cuadros.
+    En caso de que la pelota toque uno de los cuadros, el cuadro dejar� de mostrarse y se deber� actualizar su estado.
+  */
+  ball_touch_square(level);
+      
+  // Verificamos si la pelota ha tocado la pala.
+  // ball_touch_padddle();
+    
+  // Verificamos la condici�n de finalizaci�n de la partida, es decir, que la pelota llegue al fondo del rect�ngulo del canvas o que se hayan tocados todos ls cuadros.
+  end_game = end_of_game();
+
+  //Se avanza la posici�n
+  x += dx;
+  y += dy;
+
+  // Dinujamos la paleta. Utilizamos la funci�n auxiliar square.
+  square("#333", paddleX, paddleY, 90, 30 );	
+
+}	
+/*
+Funci�n para inicializar el juego.
+Esta funci�n puede ser modificada cuando se a�adan los controles para inicializar y pausar el juego.
+*/
+function init_game(){
+  canvas = document.getElementById("canvas");
+  ctx = canvas.getContext("2d"); //Siempre requerida
+  interval = 15;
+
+  // Posici�n inicial de la pelota.
+  x = 300;
+  y = 500;
+
+  // Velocidad de desplazamiento de la pelota
+  dx = 5;
+  dy = -5;
+
+  // Posici�n inicial de la paleta.
+  paddleX = 300;
+  paddleY = 500;
+
+  // El juego se inicia en el nivel 1.
+  level = 1;
+
+  // Pintamos el Canvas.
+  draw();
+
+  // Cremoas un invertablo para dibujar el canvas cada 15 milisegundos ( que es periodo defenido para el intervalo ).
+interval = setInterval(draw, interval);
+
+}
+
+function loadData () {
+  $.ajax({
+    type: 'GET',
+    url: "http://camisetas24h.com/PRA2/levels_cross.php",
+    dataType: "jsonp",
+    success: function (data){
+      squares_level1 = data[0]; // First Level
+      squares_level2 = data[1]; // Second Level
+    },
+    error:function (xhr, ajaxOptions, thrownError){
+      console.alert(xhr.status);
+      console.alert(thrownError);
+    }
+  });
+}
---
+++ src/levels.php
@@ -0,0 +1,338 @@
+<?php 
+
+$arr_level1 = array(
+    array( 
+		"color" => "#ccc", 
+		"x" => "0",
+		"y" => "0",
+		"w" => "60",
+		"h" => "30",
+		"active" => true
+    ),
+
+    array(
+		"color" => "#f00", 
+		"x" => "60",
+		"y" => "0",
+		"w" => "60",
+		"h" => "30",
+		"active" => true
+    ),
+
+    array(
+		"color" => "#0f0", 
+		"x" => "120",
+		"y" => "0",
+		"w" => "60",
+		"h" => "30",
+		"active" => true
+    ),
+
+    array(
+		"color" => "#00f", 
+		"x" => "180",
+		"y" => "0",
+		"w" => "60",
+		"h" => "30",
+		"active" => true	
+    ),
+
+    array(
+		"color" => "#ff0", 
+		"x" => "240",
+		"y" => "0",
+		"w" => "60",
+		"h" => "30",
+		"active" => true	
+    ),    
+
+    array(
+		"color" => "#f0f", 
+		"x" => "300",
+		"y" => "0",
+		"w" => "60",
+		"h" => "30",
+		"active" => true	
+    ),
+
+    array(
+		"color" => "#0ff", 
+		"x" => "360",
+		"y" => "0",
+		"w" => "60",
+		"h" => "30",
+		"active" => true	
+    ),
+
+    array(
+		"color" => "#000", 
+		"x" => "420",
+		"y" => "0",
+		"w" => "60",
+		"h" => "30",
+		"active" => true	
+    ),
+
+    array(
+		"color" => "#369", 
+		"x" => "480",
+		"y" => "0",
+		"w" => "60",
+		"h" => "30",
+		"active" => true	
+    ),
+
+
+    array(
+		"color" => "#963", 
+		"x" => "540",
+		"y" => "0",
+		"w" => "60",
+		"h" => "30",
+		"active" => true	
+    ),
+
+
+    array(
+		"color" => "#391", 
+		"x" => "120",
+		"y" => "30",
+		"w" => "60",
+		"h" => "30",
+		"active" => true	
+    ),
+
+    array(
+		"color" => "#987", 
+		"x"=> "180",
+		"y"=> "30",
+		"w"=> "60",
+		"h"=> "30",
+		"active"=> true	
+    ),
+
+    array(
+		"color" => "#123", 
+		"x" => "240",
+		"y" => "30",
+		"w" => "60",
+		"h" => "30",
+		"active" => true	
+    ),
+
+    array(
+		"color" => "#912", 
+		"x" => "300",
+		"y" => "30",
+		"w" => "60",
+		"h" => "30",
+		"active" => true	
+    ),
+
+    array(
+		"color" => "#51a", 
+		"x" => "360",
+		"y" => "30",
+		"w" => "60",
+		"h" => "30",
+		"active" => true
+    ),
+
+    array(
+		"color" => "#915", 
+		"x" => "420",
+		"y" => "30",
+		"w" => "60",
+		"h" => "30",
+		"active" => true	
+    ),
+	
+	array(
+		"color" => "#158", 
+		"x" => "240",
+		"y" => "60",
+		"w" => "60",
+		"h" => "30",
+		"active" =>  true	
+	),
+	
+	array(
+		"color" => "#972", 
+		"x" => "300",
+		"y" => "60",
+		"w" => "60",
+		"h" => "30",
+		"active" => true
+	)	
+);
+
+$arr_level2 = array(
+
+	array(
+		"color" => "#ccc", 
+		"x" => "0",
+		"y" => "0",
+		"w" => "60",
+		"h" => "30",
+		"active" => true
+	),
+
+	array(
+		"color" => "#f00", 
+		"x" => "60",
+		"y" => "0",
+		"w" => "60",
+		"h" => "30",
+		"active" => true
+	),
+	
+	array(
+		"color" => "#0f0", 
+		"x" => "120",
+		"y" => "0",
+		"w" => "60",
+		"h" => "30",
+		"active" => true
+	),
+	
+	array(
+		"color" => "#00f", 
+		"x" => "180",
+		"y" => "0",
+		"w" => "60",
+		"h" => "30",
+		"active" => true	
+	),
+
+	array(
+		"color" => "#ff0", 
+		"x" => "240",
+		"y" => "0",
+		"w" => "60",
+		"h" => "30",
+		"active" => true	
+	),
+	
+	array(
+		"color" => "#f0f", 
+		"x" => "300",
+		"y" => "0",
+		"w" => "60",
+		"h" => "30",
+		"active" => true	
+	),
+	
+	array(
+		"color" => "#0ff", 
+		"x" => "360",
+		"y" => "0",
+		"w" => "60",
+		"h" => "30",
+		"active" => true	
+	),
+	
+	array(
+		"color" => "#000", 
+		"x" => "420",
+		"y" => "0",
+		"w" => "60",
+		"h" => "30",
+		"active" => true	
+	),
+	
+	array(
+		"color" => "#369", 
+		"x" => "480",
+		"y" => "0",
+		"w" => "60",
+		"h" => "30",
+		"active" => true	
+	),
+	
+	array(
+		"color" => "#963", 
+		"x" => "540",
+		"y" => "0",
+		"w" => "60",
+		"h" => "30",
+		"active" => true	
+	),
+	
+	array(
+		"color" => "#391", 
+		"x" => "0",
+		"y" => "30",
+		"w" => "60",
+		"h" => "30",
+		"active" => true		
+	),
+	
+	array(
+		"color" => "#987", 
+		"x" => "60",
+		"y" => "30",
+		"w" => "60",
+		"h" => "30",
+		"active" => true	
+	),
+	
+	array(
+		"color" => "#123", 
+		"x" => "120",
+		"y" => "30",
+		"w" => "60",
+		"h" => "30",
+		"active" => true	
+	),	
+			
+	array(
+		"color" => "#912", 
+		"x" => "0",
+		"y" => "60",
+		"w" => "60",
+		"h" => "30",
+		"active" => true	
+	),	
+	array(
+		"color" => "#51a", 
+		"x" => "420",
+		"y" => "30",
+		"w" => "60",
+		"h" => "30",
+		"active" => true
+	),	
+	array(
+		"color" => "#915", 
+		"x" => "480",
+		"y" => "30",
+		"w" => "60",
+		"h" => "30",
+		"active" => true
+	),	
+	array(
+		"color" => "#158", 
+		"x" => "540",
+		"y" => "30",
+		"w" => "60",
+		"h" => "30",
+		"active" => true	
+	),	
+	array(
+		"color" => "#972", 
+		"x" => "540",
+		"y" => "60",
+		"w" => "60",
+		"h" => "30",
+		"active" => true
+	)
+	
+	
+	
+);
+
+$levels = array();
+array_push( $levels, $arr_level1 );
+array_push( $levels, $arr_level2 );
+echo json_encode($levels);
+
+?>
